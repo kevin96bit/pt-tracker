f42ad5b33180372a25b618b4b9d9836a9091abf3
kevin <kevin96imerti@outlook.it>
2024-04-21 18:30:40 +0200

    Inizio bozza pagina contatti (da rivedere la grafica)

--- src/app/contatti/page.tsx
+++ src/app/contatti/page.tsx
@@ -1,5 +1,24 @@
 const page = () => {
-  return <div>contatti</div>;
+  return (
+    <section className="flex items-center justify-center">
+      <div className="w-3/4 flex flex-col h-screen items-center justify-center">
+        <div className="w-3/4 border h-2/3">
+          <div className="w-full h-14 bg-blue-400 ">
+            <div className=" w-full h-full flex justify-end items-center">
+              <div className="w-20 h-4 bg-slate-300 rounded-md mr-12"></div>
+              <div className="w-20 h-4 bg-slate-300 rounded-md mr-12"></div>
+              <div className="w-20 h-4 bg-slate-300 rounded-md mr-12"></div>
+              <div className="w-20 h-4 bg-slate-300 rounded-md mr-12"></div>
+            </div>
+          </div>
+          <div className="flex w-full h-full">
+            <div className="w-[50%] border"><p className="text-7xl mt-3">Contattami</p></div>
+            <div className="w-[50%] border">2</div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default page;
