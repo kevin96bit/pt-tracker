afbf331d9bb51c75838eeac89d09c0eb6d339650
kevin <kevin96imerti@outlook.it>
2024-05-07 11:45:52 +0200

    commentato pagine : libe.tsx, azioni.tsx,logout.tsx ( dentro auth , rimessa navbar senza sessioni)

--- src/app/(auth)/logout/page.tsx
+++ src/app/(auth)/logout/page.tsx
@@ -1,13 +1,13 @@
-import { logout } from "@/azioni";
-import { Button } from "@/components/ui/button";
+// import { logout } from "@/azioni";
+// import { Button } from "@/components/ui/button";
 
-const LogoutForm = () => {
-  return (
-    <form action={logout}>
+// const LogoutForm = () => {
+//   return (
+//     <form action={logout}>
         
-      <Button>Logout </Button>
-    </form>
-  );
-};
+//       <Button>Logout </Button>
+//     </form>
+//   );
+// };
 
-export default LogoutForm;
+// export default LogoutForm;
